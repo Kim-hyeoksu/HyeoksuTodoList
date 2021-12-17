977513b643c57c03a79e596c36948d7d14b4ed56
Kim-hyeoksu <70899438+Kim-hyeoksu@users.noreply.github.com>
2021-12-17 11:02:45 +0900

    Add files via upload

---
+++ assets/js/user.js
@@ -0,0 +1,24 @@
+function getUserName() {
+  const userName = document.querySelector('main .todoListContainer .userNameInput');
+  if(localStorage.getItem('userName')) {
+    userName.outerHTML = `<span class="userName" onClick="deleteUserName()">${JSON.parse(localStorage.getItem('userName'))}</span>`
+  }
+}
+
+function setUserName() {
+  const userName = document.querySelector('main .todoListContainer .userNameInput');
+  if(userName.value != "") {
+    localStorage.setItem('userName', JSON.stringify(userName.value))
+  }
+  
+  getUserName();
+}
+
+function deleteUserName() {
+  const userName = document.querySelector('main .todoListContainer .userName')
+  localStorage.removeItem('userName')
+  userName.outerHTML = `<input type="text" class="userNameInput"
+  onkeyup="if(window.event.keyCode==13){setUserName()}"></input>`
+}
+
+export {getUserName, setUserName, deleteUserName}
